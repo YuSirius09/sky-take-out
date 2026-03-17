7fab3ec72815c21a0d84b9ef542e13f4a8a9ecca
yu sirius <1114645038@qq.com>
2026-03-18 00:06:46 +0800

    day4-4 修改套餐

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -50,4 +50,10 @@ public interface SetmealMapper {
      */
     @Delete("delete from setmeal where id = #{id}")
     void deleteById(Long setmealId);
+
+    /**
+     * 修改套餐表
+     * @param setmeal
+     */
+    void update(Setmeal setmeal);
 }
